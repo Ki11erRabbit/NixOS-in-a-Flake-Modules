5ce2825400abd99dd9c59dc07bc21b3e43c62219
ki11errabbit <ki11errabbit@posteo.net>
2025-09-30 09:40:30 -0600

    initial commit with basic modules.nix functionality

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "A small reimplementation of nixos/lib/modules.nix as a flake for NixOS in a Flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.05";
+  };
+
+  outputs = { self, nixpkgs, ... }: 
+  let 
+    lib = nixpkgs.lib;
+    # like in nixpkgs/lib/modules.nix
+    mkIf = cond: attrs: if cond then attrs else {};
+    
+    mkMerge = list: lib.foldl' (a: b: lib.recursiveUpdate a b) {} list;
+    
+    evalModules = { modules, specialArgs ? {}, pkgs }:
+        let 
+            evaluated = map (m: 
+                if builtins.isFunction m
+                then m ({ inherit pkgs lib; } // specialArgs)
+                else m
+            ) modules;
+
+            merged = mkMerge evaluated;
+        in merged;
+  in {
+    lib = {
+        inherit evalModules mkIf mkMerge;
+    };
+
+  };
+}
