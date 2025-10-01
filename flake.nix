f63c454e2fc9eb166c8f6527083e711e592954fa
ki11errabbit <ki11errabbit@posteo.net>
2025-09-30 20:55:52 -0600

    added extra step of creating directories

--- flake.nix
+++ flake.nix
@@ -25,13 +25,14 @@
         in merged;
 
     mkOption = { pkgs, packages ? [], files ? [], hooks ? [] }: let 
+        dirHooks = map (dir: "\nmkdir -p ${dir}") (lib.unique (map (file: file.location) files));
         textFiles = map (file: pkgs.writeTextFile {
                     name = file.name;
                     text = file.text;
                     destination = "${file.location}/${file.name}";
                 }) files;
         fileHooks = map (textFile: "\nln -sf ${textFile}/${textFile.destination} ${textFile.destination}") textFiles;
-        fileHook = if fileHooks == [] then "" else "\n${lib.concatStringsSep " " fileHooks}";
+        fileHook = if fileHooks == [] then "" else "\n${lib.concatStringSep " " dirHooks}\n${lib.concatStringsSep " " fileHooks}";
         optionHooks = map (hook: "\n${hook}") hooks;
         allHooks = fileHook + lib.concatStrings optionHooks ;
         pkg = packages;
